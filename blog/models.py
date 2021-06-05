4e335c930a3e9385e3b9c96fd2cc8c5291303ad3
unknown <onifaie@gmail.com>
2021-06-05 18:20:47 +0300

    addshortcutkey

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,17 @@ from django.db import models
 
 # Create your models here.
 from .models import * 
+from datetime import date
+from django.contrib.auth.models import User
+
+
 
 class blog(models.Model):
-    pass
+    title=models.CharField(max_length=100,editable=True)
+    discrption=models.TextField(max_length=4000)
+    dateAdd=models.DateField(auto_created=True)
+    image=models.ImageField(upload_to='img/')
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.title,self.user
